1935ef19ec404d004ef52a62c6c0fd2778cc892d
Дмитрий <di1983@mail.ru>
2023-11-29 10:26:56 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut age: isize = 0x0A10;
+    //let s1: &str = "My";
+    let sym: char = 'A';
+    let b: bool = true;
+    let num: f64 = 2.34;
+    let name = "Rust";
+
+    println!("{} age = {}!", name, age);
+
+    for i in 1..10
+    {
+        age += i;
+    }
+
+    print!("{} age = {}! {} {} {}", name, age, num, b, sym);
 }
